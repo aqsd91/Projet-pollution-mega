052e2065175741db8bde57bc25a0c4c894a625bd
aqsd91 <francois.yuio91@gmail.com>
2023-11-28 17:09:04 +0100

    Radio + commentaires

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
 #include <Wire.h>     // Import de la bibliothèque de gestion de l'I2C
 #include "rgb_lcd.h"  // Import du fichier de gestion de l'afficheur LCD
-#include "DHT.h"
+#include "DHT.h"      // Import du fichier du capteur DHT20
 #include <Arduino_JSON.h>
-
+//======= Définition variables capteurs
 #define DHTTYPE DHT20  // DHT 20
 rgb_lcd lcd;
 int Analog_Bruit = A0;  // Entrée analogique
@@ -24,7 +24,7 @@ bool btn = false;
 //========== REGLAGES
   uint32_t timerDelay     = 1000;       // Fréquence d'envoi vers le NODE
   uint32_t speedSerial    = 115200;     // Vitesse moniteur série
-  uint32_t speedSerial1   = 115200;     // Vitesse envoi des données vers le NODE
+  uint32_t speedSerial1   = 9600;     // Vitesse envoi des données vers le NODE
   
 
 
@@ -55,7 +55,7 @@ bool btn = false;
     #define SETUP_VIR_END(varName) varName++;}
 
 
-//========== PROTOTYPES DE FONCTIONS
+//========== Déclarations fonctions
 String getSensorReadings();
 
 void gereralarme();
@@ -70,6 +70,12 @@ void setup() {
   Serial.begin(speedSerial);  while(!Serial);
   Serial1.begin(speedSerial1);while(!Serial1);  
   Serial.println("Serials:ok");
+  //======== Initialisation Radio
+  int setPin = 7; // Car la broche SET du HC12 est sur 7
+  pinMode(setPin, OUTPUT); // La broche 7 est une sortie à piloter
+  digitalWrite(setPin,LOW); // On met SET en mode changement canal
+  Serial1.print(F("AT+DEFAULT"));delay(100); // Paramètres usine
+  Serial1.print(F("AT+C032"));
   //======== Initialisation Capteurs
   pinMode(Analog_Bruit, INPUT);
   pinMode(Digital_Bruit, INPUT);
@@ -86,8 +92,14 @@ void setup() {
 void loop() {
   alarme = 0;
   //----- Récupération des données des capteurs la MEGA (capteurs locaux)      
-      data_mega_5  = random(0,10000);                       // Exemple nbr 4 octets non signés (au hasard)
-      data_mega_6  = "<span style='color:green;'>GREEN<span>";  // Exemple String avec injection html
+    data_mega_5  = random(0,10000);                       // Exemple nbr 4 octets non signés (au hasard)
+    data_mega_6  = "<span style='color:green;'>GREEN<span>";  // Exemple String avec injection html
+    // Valeur déboguage sèrie
+    temp_mega = random(0,1000);
+    hum_mega = random(0,1000);
+    poll_mega = random(0,1000);
+    bruit_mega = random(0, 1000);
+    
     gererBruit();
     gererTempHum();
     gererPollution();
@@ -101,6 +113,7 @@ void loop() {
 
 }
 
+//======== Gestion de l'alarme
 void gereralarme() {
   for (int i = 0; i < 20; i++) {
     digitalWrite(LED1, HIGH);
@@ -112,6 +125,7 @@ void gereralarme() {
   }
 }
 
+//======== Création de la chaine JSON
 String getSensorReadings() {
   //Exemple JSON {"DATA1":"1234","DATA2":"3.14","DATA3":"Hello world","DATA4":"-12345678","DATA5":"123456789","DATA6":"a"}
   // Les noms des étiquettes DATAx doivent être les mêmes que dans l'id html ! (exemple avec DATAx)
@@ -126,27 +140,27 @@ String getSensorReadings() {
   return jsonString;
 }
 
+//======== Gestion capteur de bruit
 void gererBruit(){
-  // Capteur de Bruit
   bruit_mega = analogRead(Analog_Bruit) * (5.0 / 1023.0);
   bruit_digital = digitalRead(Digital_Bruit);
 
   //... et envoyées à la sortie série.
-  // Serial.print("Tension analogique:");
-  // Serial.print(Analog, 4);
-  // Serial.print("V, ");
-  // Serial.print("Limite:");
+  // debug.print("Tension analogique:");
+  // debug.print(Analog, 4);
+  // debug.print("V, ");
+  // debug.print("Limite:");
 
   // if (bruit_digital == 1) {
-  //   Serial.println(" atteinte");
+  //   debug.println(" atteinte");
   // } else {
-  //   Serial.println(" pas encore atteinte");
+  //   debug.println(" pas encore atteinte");
   // }
-  // Serial.println("----------------------------------------------------------------");
+  // debug.println("----------------------------------------------------------------");
 }
 
+//======== Gestion capteur de température/humidité
 void gererTempHum(){
-  // Capteur de temperature/humidite  
   float temp_hum_val[2] = { 0 };
   if (!dht.readTempAndHumidity(temp_hum_val)) {
     temp_mega = temp_hum_val[1];
@@ -163,6 +177,7 @@ void gererTempHum(){
   }
 }
 
+//======== Gestion capteur pollution
 void gererPollution(){
   // Capteur de pollution
   float sensor_volt;
@@ -170,11 +185,12 @@ void gererPollution(){
   sensorValue = analogRead(A2);
   sensor_volt = sensorValue / 1024 * 5.0;
   poll_mega = sensor_volt;
-  // Serial.print("sensor_volt = ");
-  // Serial.print(sensor_volt);
-  // Serial.println("V");
+  // debug.print("sensor_volt = ");
+  // debug.print(sensor_volt);
+  // debug.println("V");
 }
 
+//======== Gestion de l'écran LCD
 void gererLCD(){
   // Ecran Bruit
   if (bruit_digital == 1) {
@@ -198,13 +214,13 @@ void gererLCD(){
 
   delay(1000);
   
-  // Ecran humidite
+  // Ecran humidité
   if (hum_mega > 75) {
     setLCD(255, 0, 0, "Humidite", String(hum_mega) + " %");
   } else {
     setLCD(0, 255, 0, "Humidite", String(hum_mega) + " %");
   }
-
+  // Alarme humidité
   if ((bruit_digital == 1 || hum_mega > 75 || temp_mega > 35 || poll_mega > 0.5) && alarme!=2) {
     alarme = 1;
   } else if (alarme!=2) {
@@ -220,13 +236,13 @@ void gererLCD(){
 
   delay(1000);
   
-  // Ecran Temperature
+  // Ecran temperature
   if (temp_mega > 35) {
     setLCD(255, 0, 0, "Temperature", String(temp_mega) + " *C");
   } else {
     setLCD(0, 255, 0, "Temperature", String(temp_mega) + " *C");
   }
-
+  // Alarme temperature
   if ((bruit_digital == 1 || hum_mega > 75 || temp_mega > 35 || poll_mega > 0.5) && alarme!=2) {
     alarme = 1;
   } else if (alarme!=2) {
@@ -247,7 +263,7 @@ void gererLCD(){
   } else {
     setLCD(0, 255, 0, "Pollution", String(poll_mega));
   }
-
+  // Alarme pollution
   if ((bruit_digital == 1 || hum_mega > 75 || temp_mega > 35 || poll_mega > 0.5) && alarme!=2) {
     alarme = 1;
   } else if (alarme!=2) {
@@ -264,7 +280,7 @@ void gererLCD(){
   delay(1000);
 
 }
-
+//======== Édition de l'écran LCD
 void setLCD(int R, int G, int B, String texte, String valeur){
   lcd.setRGB(R, G, B);     // Met le rétro-éclairage en rouge (les 3 valeurs = Rouge,Vert,Bleu)
   lcd.setCursor(0, 0);     // Place le curseur à la colonne 0 et ligne 0 (la 1ère ligne est la ligne 0)
